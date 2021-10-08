fd4df7116afdeac9e1efe4c153537eefb8f7d6cb
Utsumi <51894217+UtsumiYoji@users.noreply.github.com>
2021-10-08 22:06:47 +0900

    Update transactionConnection.py
    データ処理を関数化
    normal, internal, token, nftの処理を暫定追加

--- _program/transactionConnection.py
+++ _program/transactionConnection.py
@@ -1,60 +1,63 @@
 from ExternalAPI import explorerControl
 
-def expantion(address, settingData, sBlock, eBlock):
-    #settingからデータを取り出す
-    url = settingData['scanUrl']
-    api = settingData['scanApi']
-    symbol = settingData['symbol']
-    contactAd = settingData['address']
-    
-    #スキャンのインタンスを作成
-    ins = explorerControl.explorerControl(url=url, api=api)
-    ins.setAddress(address=address)
+class transactionConnection:
 
-    #データを格納する変数
-    transaction = list()
-    errorGas = list()
+    def __init__(self):
+        self.transaction = list()
+        self.errorGas = list()
 
-    #ノーマルトランザクションを取得
-    normalTx = ins.normalTransactions(sBlock, eBlock)
-    if normalTx['message'] == 'OK':
-        #データ取得が成功したらデータの数だけループ
-        for data in normalTx['result']:
-            if data['isError'] == '1':
-                #エラーだったらガス代だけ取得
-                errorGas += [
-                    data['blockNumber'], data['timeStamp'], data['hash'], 
-                    data['gas'], data['gasPrice'],
-                ]
+    def dataProcessing(self, data, symbol, contract, value):
+        if data['isError'] == '1':
+            # エラーだったらガス代だけ取得
+            self.errorGas += [
+                data['blockNumber'], data['timeStamp'], data['hash'],
+                data['gas'], data['gasPrice'],
+            ]
 
-            else:
-                #エラーじゃないならデータに追加
-                transaction += [
-                    data['blockNumber'], data['timeStamp'], data['hash'], data['from'],
-                    data['to'], data['value'], symbol, contactAd, 
-                ]
-    
-    #インターナルトランザクションを取得
-    internalTX = ins.internalTransactions(sBlock, eBlock)
-    if internalTX['message'] == 'OK':
-        #データ取得が成功したらデータの数だけループ
-        for data in normalTx['result']:
-            if data['isError'] == '1':
-                #エラーだったらガス代だけ取得
-                errorGas += [
-                    data['blockNumber'], data['timeStamp'], data['hash'], 
-                    data['gas'], data['gasPrice'],
-                ]
+        else:
+            # エラーじゃないならデータに追加
+            self.transaction += [
+                data['blockNumber'], data['timeStamp'], data['hash'], data['from'],
+                data['to'], value, symbol, contract,
+            ]
 
-            else:
-                #エラーじゃないならデータに追加
-                transaction += [
-                    data['blockNumber'], data['timeStamp'], data['hash'], data['from'],
-                    data['to'], data['value'], symbol, contactAd, 
-                ]
-    
-    #スマートコンストラクトトランザクションを取得
-    tokenTx = ins.tokenTransactions(sBlock, eBlock)
-    nftTX = ins.tokennfttxTransactions(sBlock, eBlock)
+    def expantion(self, address, settingData, sBlock, eBlock):
+        # settingからデータを取り出す
+        url = settingData['scanUrl']
+        api = settingData['scanApi']
+        symbol = settingData['symbol']
+        contract = settingData['address']
 
-    
+        # スキャンのインタンスを作成
+        ins = explorerControl.explorerControl(url=url, api=api)
+        ins.setAddress(address=address)
+
+        # ノーマルトランザクションを取得
+        normalTx = ins.normalTransactions(sBlock, eBlock)
+        if normalTx['message'] == 'OK':
+            # データ取得が成功したらデータの数だけループ
+            for data in normalTx['result']:
+                self.dataProcessing(data, symbol, contract, data['value'])
+
+        # インターナルトランザクションを取得
+        internalTX = ins.internalTransactions(sBlock, eBlock)
+        if internalTX['message'] == 'OK':
+            # データ取得が成功したらデータの数だけループ
+            for data in normalTx['result']:
+                self.dataProcessing(data, symbol, contract, data['value'])
+
+        # スマートコンストラクトトランザクションを取得
+        tokenTx = ins.tokenTransactions(sBlock, eBlock)
+        if tokenTx['message'] == 'OK':
+            # データ取得が成功したらデータの数だけループ
+            for data in tokenTx['result']:
+                value = int(data['value']) * (10 ** -int(data['tokenDecimal']))
+                self.dataProcessing(data, data['tokenSymbol'], data['contractAddress'], value)
+
+        # NFTデータを取得
+        nftTx = ins.tokennfttxTransactions(sBlock, eBlock)
+        if nftTx['message'] == 'OK':
+            # データ取得が成功したらデータの数だけループ
+            for data in nftTx['result']:
+                value = int(data['value']) * (10 ** -int(data['tokenDecimal']))
+                self.dataProcessing(data, data['tokenSymbol'], data['contractAddress'], value)
